fefcc42e5c6ada7af40397008f23b05ecc10c538
Cristian Benavides Franco <p-etta@hotmail.com>
2021-12-10 17:07:35 -0500

    Day Six Challenges

---
+++ AdventOfCode2021/Solution.ConsoleApplication/Challenges/DaySixChallengeFactory.cs
@@ -0,0 +1,19 @@
+﻿using Solution.ConsoleApplication.Common;
+
+namespace Solution.ConsoleApplication.Challenges
+{
+    public class DaySixChallengeFactory : ChallengeFactory
+    {
+        private readonly string _sourceFile;
+
+        public DaySixChallengeFactory(string sourceFile)
+        {
+            _sourceFile = $"../../../../{sourceFile}";
+        }
+
+        public override Challenge GetChallenge()
+        {
+            return new DaySixChallenge(_sourceFile);
+        }
+    }
+}
